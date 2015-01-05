3f0271cb09429868ed895c4b099a15a68f2ef580
boris.schapira <boris.schapira@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 13:58:48 +0000

    v0.10
    git-svn-id: https://plugins.svn.wordpress.org/typofr/trunk@1060417 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- typofr.php
+++ typofr.php
@@ -5,7 +5,7 @@
  * Description: a plugin for french typography management, powered by JoliTypo
  *
  * Plugin URI: https://github.com/borisschapira/typofr
- * Version: 0.9
+ * Version: 0.10
  *
  * Author: Boris Schapira
  * Author URI: http://borisschapira.com
